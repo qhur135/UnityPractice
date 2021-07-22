1c6c8259576a5e432e9e400e1e199f13ef8f5596
qhur135 <qhur135@gmail.com>
2021-07-22 23:22:39 +0900

    feat : ball 움직임 개선 - transform

--- Ball/Assets/Scripts/Player.cs
+++ Ball/Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
 {
     public Text score;
     int curScore;
+    public float moveSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +22,7 @@ public class Player : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float jump = Input.GetAxis("Jump");
         Vector3 movement = new Vector3(horizontal,jump * 20,vertical);
-        Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(movement);
+        transform.Translate(movement*Time.deltaTime*moveSpeed);
     }
     private void OnTriggerEnter(Collider other)
     {
