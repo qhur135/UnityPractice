7a389b14c65e26a0cd49f9e26d63c073dc692418
qhur135 <qhur135@gmail.com>
2021-07-23 01:56:35 +0900

    feat : pacman item 동작 및 애니메이션

--- Pacman/Assets/Scripts/PlayerCtrl.cs
+++ Pacman/Assets/Scripts/PlayerCtrl.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCtrl : MonoBehaviour
 {
     public float moveSpeed = 3f;
+    public Text txtScore;
+    int curScore = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,12 @@ public class PlayerCtrl : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontal,0f,vertical);
         transform.Translate(movement * Time.deltaTime * moveSpeed); // 힘으로 이동하는 것이 아니라 일정한 속도로 원하는 곳으로 이동하도록 함
+    }
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag=="Item"){
+            Destroy(other.gameObject);
+            curScore+=1;
+            txtScore.text = curScore.ToString();
+        }
     }
 }
